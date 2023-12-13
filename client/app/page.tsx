06ccc818f6d6677363f505e9449deadded31e959
zakhaev26 <b422056@iiit-bh.ac.in>
2023-12-13 15:59:24 +0530

    adds page transitions to make cool cmooth page switch

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,15 +7,19 @@ import { UserAuth } from "@/auth/context/AuthContext"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 import AnimatedText from "@/components/animations/text"
+import SignpostIcon from '@mui/icons-material/Signpost';
 export default function Home() {
 
   const { user, googleSignIn, logOut } = UserAuth();
   const Router = useRouter();
 
-  useEffect(()=>{
-      console.log("user = " ,user)
-  },[user])
+  useEffect(() => {
+    console.log("user = ", user)
+  }, [user])
 
+  const handlePostRedirect = () => {
+    Router.push("/posts");
+  }
 
   const handleSignIn = async () => {
     try {
@@ -35,7 +39,13 @@ export default function Home() {
   }
 
   return (
-    <div className='mainbody'>
+    <motion.div
+    initial={{ opacity: 0, x: -20 }}
+    animate={{ opacity: 1, x: 0 }}
+    exit={{ opacity: 0, x: -20 }}
+    transition={{duration: 2}}
+    className="mainbody"
+  >
       .
       <div className='rectangle1'>
         <div className='nav'>
@@ -63,27 +73,33 @@ export default function Home() {
 
           <div className="hero_form">
 
-            {user ? (
+            {user ? ( 
               <>
-              
-              <img className="circular-img" src={user.photoURL} alt="" />
-              <AnimatedText text={user?.displayName} size="28" />
-              <AnimatedText text="Last Signed In :" size="14" />
-              <AnimatedText text={user.metadata.lastSignInTime} size="10" />
-              <button onClick={handleSignOut} type="button" className="login-with-google-btn" >
-                Sign Out
-              </button>
-              <br />
+
+                <img className="circular-img" src={user.photoURL} alt="" />
+                <AnimatedText text={user?.displayName} size="28" />
+                <AnimatedText text="Last Signed In :" size="14" />
+                <AnimatedText text={user.metadata.lastSignInTime} size="10" />
+
+                <button onClick={handlePostRedirect} type="button" className="check-posts" >
+                < SignpostIcon style={{color:"#000"}} />
+                  Check Out Posts
+                </button>
+
+                <button onClick={handleSignOut} type="button" className="login-with-google-btn sign-out" >
+                  Sign Out
+                </button>
+                <br />
               </>
             ) : (
               <>
-               <AnimatedText text="Log In / Sign Up" size="29" />
-               <br /> 
-               <AnimatedText text="Level Up with Enitiate." size="14" />
-              <button onClick={handleSignIn} type="button" className="login-with-google-btn" >
-              Sign In With Google 
-            </button>
-            </>
+                <AnimatedText text="Log In / Sign Up" size="29" />
+                <br />
+                <AnimatedText text="Level Up with Enitiate." size="14" />
+                <button onClick={handleSignIn} type="button" className="login-with-google-btn" >
+                  Sign In With Google
+                </button>
+              </>
             )}
 
           </div>
@@ -91,6 +107,6 @@ export default function Home() {
         </div>
         <div className="ellipse">ok</div>
       </div>
-    </div>
+    </motion.div>
   )
 }
--- client/app/posts/page.jsx
+++ client/app/posts/page.jsx
@@ -1,9 +1,18 @@
+"use client"
 import AnimatedText from "@/components/animations/text"
+import { duration } from "@mui/material";
+import {motion} from "framer-motion";
 const page = () => {
   return (
-    <div>
-      <AnimatedText text="Soubhik"/>
-    </div>
+    <motion.div
+    initial={{ opacity: 0, x: -20 }}
+    animate={{ opacity: 1, x: 0 }}
+    exit={{ opacity: 0, x: -20 }}
+    transition={{duration: 2}}
+  >
+      <AnimatedText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, facilis a provident facere voluptatem accusantium porro architecto animi est nobis dicta natus voluptates, inventore quo iste, deserunt libero odit vero repellat voluptatibus quia distinctio. Animi quasi recusandae hic quos tempore, dolorum molestias atque aut quaerat minima porro tempora sunt dignissimos repellat iusto vel laboriosam corporis ducimus!
+"/>
+    </motion.div>
   )
 }
 
